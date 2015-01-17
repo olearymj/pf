678b9edbea2ec8a3c99dbfdb9dda94ad206d174f
olearymj <olearymj@tcd.ie>
2015-01-17 12:46:43 +0000

    adding app homepage

---
+++ projects/sgp/app_home.php
@@ -0,0 +1,47 @@
+<?php 
+
+$home_url = 'http://local.hannah.com/moi';
+$url = $_SERVER['REQUEST_URI'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57408221-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="http://local.hannah.com/moi/js/script.js"></script>
+<script src="http://local.hannah.com/moi/js/isotope.min.js"></script>
+<script src="http://local.hannah.com/moi/js/modernizr.custom.47815.js"></script>
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" type="text/css" href="http://local.hannah.com/moi/css/normalise.css">
+<link rel="stylesheet" type="text/css" href="http://local.hannah.com/moi/css/style.css">
+<title>Jack O'Leary McNeice</title>
+</head>
+<body>
+	
+
+<div class="app_home">
+	<span class="water">
+		<a href="http://jackolearymcneice.com/projects/sgp/app/water"></a>
+	</span>
+	<span class="sun">
+		<a href="http://jackolearymcneice.com/projects/sgp/app/sun"></a>
+	</span>
+	<span class="earth">
+		<a href="http://jackolearymcneice.com/projects/sgp/app/earth"></a>
+	</span>
+	<span class="air">
+		<a href="http://jackolearymcneice.com/projects/sgp/app/air"></a>
+	</span>
+</div>
+</body>
+</html>
